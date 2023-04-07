d73e8a54fdf9f770725f3c286d198234f6deaef5
Heston Vaughan <heston.cv@gmail.com>
2023-04-07 14:26:37 -0500

    Add calculation functionality, created bugs

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
 let currentNumber = "";
 let numbers = [];
 let operators = [];
+let off = true;
 
 function clearDisplay()
 {
     const display = document.querySelector("#display-text");
+    if(off)
+    {
+        display.textContent = "";
+        return;
+    }
     display.textContent = "0";
 }
 
@@ -12,17 +18,54 @@ function clearDisplay()
 function updateDisplay(input) {
     const display = document.querySelector("#display-text");
     display.textContent = (display.textContent === "0") ? "" : display.textContent;
-    if(input === "clear")
+    
+    if(!off)
     {
-        clearDisplay();
+        if(input === "clear")
+        {
+            clearData();
+            clearDisplay();
+            return;
+        }
+        else if(input === "off")
+        {
+            off = true;
+            clearDisplay();
+            clearData();
+            return;
+        }
+        else if(input === "+" || input === "x" || input === "-" || input === "/")
+        {
+            console.log("operators: " + operators.length);
+            console.log("numbers: " + numbers.length);
+            if((operators.length <= numbers.length) && display.textContent !== "")
+            {
+                display.textContent = display.textContent + " " + input + " ";
+                return;
+            }
+            display.textContent = "0";
+            return;
+        }
+    }
+    else
+    {
+        if(input === "clear")
+        {
+            off = false;
+            clearData();
+            clearDisplay();
+            return;
+        }
         return;
     }
-    else if(input === "+" || input === "x" || input === "-" || input === "/")
+    if(currentNumber.length < 10 && display.textContent.length < 21)
     {
-        display.textContent = display.textContent + " " + input + " ";
-        return;
+        if(input !== "=")
+        {
+            display.textContent = display.textContent + input;
+        }
     }
-    display.textContent = display.textContent + input;
+    return;
 }
 
 function clearData()
@@ -35,6 +78,7 @@ function clearData()
 
 function updateData(input)
 {
+    const display = document.querySelector("#display-text");
     console.log("input: " + input);
     if(input === "clear")
     {
@@ -43,10 +87,13 @@ function updateData(input)
     }
     else if(input === "+" || input === "x" || input === "-" || input === "/")
     {
-
-        numbers.push(currentNumber);
-        currentNumber = "";
-        operators.push(input);
+        if(operators.length <= numbers.length && display.textContent !== "0")
+        {
+            numbers.push(currentNumber);
+            currentNumber = "";
+            operators.push(input);
+            return;
+        }
         return;
     }
     else if(input === "=")
@@ -57,8 +104,22 @@ function updateData(input)
         console.table(numbers);
         console.table(operators);
         calculate();
+        return;
     }
-    currentNumber = currentNumber + input;
+
+    if(currentNumber.length < 10 && display.textContent.length < 21)
+    {
+        currentNumber = currentNumber + input;
+    }
+    
+}
+
+function displayAnswer(answer)
+{
+    clearData();
+    clearDisplay();
+    const display = document.querySelector("#display-text")
+    display.textContent = `${answer}`;
 }
 
 function add(num1, num2)
@@ -67,18 +128,23 @@ function add(num1, num2)
 }
 function subtract(num1, num2)
 {
-    return num1 - num2;
+    return parseFloat(num1) - parseFloat(num2);
 }
 function multiply(num1, num2)
 {
     return parseFloat(num1) * parseFloat(num2);
+}
+function divide(num1, num2)
+{
+    return parseFloat(num1) / parseFloat(num2);
 }
 
 
 function calculate()
 {
     let index;
-    for(let i = 0; i < operators.length; i++)
+    let length = operators.length;
+    for(let i = 0; i < length; i++)
     {
         if((index = operators.indexOf("x")) !== -1)
         {
@@ -89,8 +155,37 @@ function calculate()
             console.log(numbers);
             console.log(operators);
         }
-        // else if((index = operators.indexOf("/")) !== -1)
+        else if((index = operators.indexOf("/")) !== -1)
+        {
+            let calculation = divide(numbers[index], numbers[index+1]);
+            numbers.splice(index, 2);
+            numbers.splice(index, 0, `${calculation}`);
+            operators.splice(index, 1);
+            console.log(numbers);
+            console.log(operators);
+        }
+        else if((index = operators.indexOf("+")) !== -1)
+        {
+            let calculation = add(numbers[index], numbers[index+1]);
+            numbers.splice(index, 2);
+            numbers.splice(index, 0, `${calculation}`);
+            operators.splice(index, 1);
+            console.log(numbers);
+            console.log(operators);
+        }
+        else if((index = operators.indexOf("-")) !== -1)
+        {
+            let calculation = subtract(numbers[index], numbers[index+1]);
+            numbers.splice(index, 2);
+            numbers.splice(index, 0, `${calculation}`);
+            operators.splice(index, 1);
+            console.log(numbers);
+            console.log(operators);
+        }
     }
+    let answer = numbers[0];
+    displayAnswer(answer);
+    currentNumber = answer;
 }
 
 //check for button clicks
@@ -99,7 +194,14 @@ console.log(buttons);
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         console.log(e.target.id);
-        updateDisplay(`${e.target.id}`);
-        updateData(`${e.target.id}`);
+        if(!off)
+        {
+            updateData(`${e.target.id}`);
+            updateDisplay(`${e.target.id}`);
+        }
+        else
+        {
+            updateDisplay(`${e.target.id}`);
+        }
     });
 });
